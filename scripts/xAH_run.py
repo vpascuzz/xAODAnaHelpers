3630e404b5d92aa50aef63086d8f321f6677f557
Giordon Stark <kratsg@gmail.com>
2016-03-18 15:49:49 -0500

    this should add the load functionality for SampleHandler

--- scripts/xAH_run.py
+++ scripts/xAH_run.py
@@ -51,7 +51,7 @@ class _HelpAction(argparse.Action):
         print("That is not a valid subsection. Chose from {{{0:s}}}".format(','.join(available_groups)))
     parser.exit()
 
-try:  
+try:
   __version__ = subprocess.check_output(["git", "describe", "--always"], cwd=os.path.dirname(os.path.realpath(__file__))).strip()
   __short_hash__ = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"], cwd=os.path.dirname(os.path.realpath(__file__))).strip()
 except:
@@ -97,6 +97,7 @@ parser.add_argument('--inputList', dest='use_inputFileList', action='store_true'
 parser.add_argument('--inputTag', dest='inputTag', default="", help='A wildcarded name of input files to run on.')
 parser.add_argument('--inputDQ2', dest='use_scanDQ2', action='store_true', help='If enabled, will search using DQ2. Can be combined with `--inputList`.')
 parser.add_argument('--inputEOS', action='store_true', dest='use_scanEOS', default=False, help='If enabled, will search using EOS. Can be combined with `--inputList and inputTag`.')
+parser.add_argument('--inputSH', action='store_true', dest='use_SH', default=False, help='If enabled, will assume the input file is a directory of ROOT files of saved SH instances to use. Call SH::SampleHandler::load() on it.')
 parser.add_argument('-v', '--verbose', dest='verbose', action='count', default=0, help='Enable verbose output of various levels. Can increase verbosity by adding more ``-vv``. Default: no verbosity')
 
 # first is the driver common arguments
@@ -288,7 +289,9 @@ if __name__ == "__main__":
     sh_all = ROOT.SH.SampleHandler()
 
     # this portion is just to output for verbosity
-    if args.use_inputFileList:
+    if args.use_SH:
+      xAH_logger.info("\t\tReading in file(s) using SH::SampleHandler::load(dir)")
+    elif args.use_inputFileList:
       xAH_logger.info("\t\tReading in file(s) containing list of files")
       if args.use_scanDQ2:
         xAH_logger.info("\t\tAdding samples using scanDQ2")
@@ -305,7 +308,9 @@ if __name__ == "__main__":
         xAH_logger.info("\t\tAdding samples using scanDir")
 
     for fname in args.input_filename:
-      if args.use_inputFileList:
+      if args.use_SH:
+        sh_all.load(fname)
+      elif args.use_inputFileList:
         if (args.use_scanDQ2 or use_scanEOS):
           with open(fname, 'r') as f:
             for line in f:
